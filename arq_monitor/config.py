b62e5e849fcb4643c8d0bb5b91193339cd78dfad
Piotr Patrzyk <piotr@patrzyk.me>
2025-01-18 14:10:47 +0100

    .env config

--- arq_monitor/config.py
+++ arq_monitor/config.py
@@ -3,7 +3,8 @@ Config
 """
 
 from arq.connections import RedisSettings
+from starlette.config import Config
 
-# TODO take from env var / option, 6379 default
-REDIS_ADDRESS = "redis://localhost:6380"
+config = Config(".env")
+REDIS_ADDRESS = config("REDIS_ADDRESS", default="redis://localhost:6379")
 ARQ_CONN_CONFIG = RedisSettings.from_dsn(REDIS_ADDRESS)
