d42faadf739b67761fcdd386c31106a669f8b26b
Piotr Patrzyk <piotr@patrzyk.me>
2025-01-19 14:04:30 +0100

    terminal css

--- src/arq_monitor/config.py
+++ src/arq_monitor/config.py
@@ -6,6 +6,7 @@ from arq.connections import RedisSettings
 from jinja2 import Environment, FileSystemLoader
 import os
 from starlette.config import Config
+from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
@@ -17,10 +18,13 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 assets_dir = os.path.join(app_dir, "frontend/assets")
 template_dir = os.path.join(app_dir, "frontend/templates")
 
+def app_context(request: Request) -> dict:
+    return {"request": request, }
+
 jinja_env = Environment(
     autoescape=True,
     loader=FileSystemLoader(searchpath=template_dir)
 )
-TEMPLATES = Jinja2Templates(env=jinja_env)
+TEMPLATES = Jinja2Templates(env=jinja_env, context_processors=[app_context, ],)
 
 STATIC = StaticFiles(directory=assets_dir)
