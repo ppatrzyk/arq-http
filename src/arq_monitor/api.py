68c88e286574cef54d68985c7d5e5aea58fead66
Piotr Patrzyk <piotr@patrzyk.me>
2025-01-22 16:35:11 +0100

    api docs

--- src/arq_monitor/api.py
+++ src/arq_monitor/api.py
@@ -6,12 +6,50 @@ from starlette.exceptions import HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+from starlette.schemas import SchemaGenerator
 
+from .config import logger, TEMPLATES
 from .utils import create_new_job, get_jobs_data
+
+schemas = SchemaGenerator(
+    {"openapi": "3.0.0", "info": {"title": "arq-monitor", "version": "0.1"}}
+)
 
 async def get_jobs(request: Request):
     """
-    Get list of jobs
+    responses:
+        200:
+            description: List of jobs.
+            content:
+                application/json:
+                    schema:
+                        type: object
+                        properties:
+                            status:
+                                type: string
+                                description: status
+                            updated_at:
+                                type: string
+                                description: ISO formatted time
+                            results:
+                                type: object
+                                description: processed jobs
+                            queues:
+                                type: object
+                                description: jobs in given queue
+        500:
+            description: Server error.
+            content:
+                application/json:
+                    schema:
+                        type: object
+                        properties:
+                            status:
+                                type: string
+                                description: status
+                            detail:
+                                type: string
+                                description: info on error
     """
     try:
         data = await get_jobs_data(arq_conn=request.state.arq_conn)
@@ -25,7 +63,68 @@ async def get_jobs(request: Request):
 
 async def create_job(request: Request):
     """
-    Get status of existing job
+    requestBody:
+        required: true
+        content:
+            application/json:
+                schema:
+                    type: object
+                    required:
+                        - function
+                        - _queue_name
+                    properties:
+                        function:
+                            type: string
+                            description: function name
+                        args:
+                            type: array
+                            description: args to be passed to function
+                        kwargs:
+                            type: object
+                            description: kwargs to be passed to function
+                        _queue_name:
+                            type: string
+                            description: queue name
+    responses:
+        201:
+            description: Job created.
+            content:
+                application/json:
+                    schema:
+                        type: object
+                        properties:
+                            status:
+                                type: string
+                                description: status
+                            job_id:
+                                type: string
+                                description: job_id of created job
+        400:
+            description: Job not created.
+            content:
+                application/json:
+                    schema:
+                        type: object
+                        properties:
+                            status:
+                                type: string
+                                description: status
+                            detail:
+                                type: string
+                                description: info on error
+        500:
+            description: Server error.
+            content:
+                application/json:
+                    schema:
+                        type: object
+                        properties:
+                            status:
+                                type: string
+                                description: status
+                            detail:
+                                type: string
+                                description: info on error
     """
     try:
         data = await request.json()
@@ -44,7 +143,28 @@ async def create_job(request: Request):
         raise HTTPException(status_code=500, detail=str(e))
     return response
 
+def openapi_schema(request: Request):
+    """
+    Api schema
+    """
+    return schemas.OpenAPIResponse(request=request)
+
+def docs(request: Request):
+    """
+    Automatic docs based on api schema
+    """
+    response = TEMPLATES.TemplateResponse(
+        request=request,
+        name="docs.html.jinja",
+        context={}
+    )
+    return response
+
 api_routes = [
+    # api
     Route(path='/jobs', endpoint=get_jobs, methods=["GET", ], name="get_jobs"),
     Route(path='/jobs', endpoint=create_job, methods=["POST", ], name="create_job"),
+    # docs
+    Route(path="/docs", endpoint=docs, include_in_schema=False),
+    Route(path="/schema", endpoint=openapi_schema, include_in_schema=False),
 ]
